8857fac480f721c30111c302a299cda7b57d0a65
nelisam <nelisamaqungo@gmail.com>
2020-04-24 23:46:44 +0200

    updated project and ejs edits

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const articleRouter = require("./routes/articles");
 const methodOverride = require("method-override");
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 //Hook up DB
 mongoose.connect("mongodb://localhost/food_blog", {
   useNewUrlParser: true,
@@ -30,6 +36,20 @@ app.get("/", async (req, res) => {
   //         "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
   //     },
   //     {
+  //       title: "Test Article 2",
+  //       createdAt: new Date(),
+  //       description: "Test description 2",
+  //       image:
+  //         "https://images.pexels.com/photos/1431305/pexels-photo-1431305.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  //     },
+  //   {
+  //       title: "Test Article 2",
+  //       createdAt: new Date(),
+  //       description: "Test description 2",
+  //       image:
+  //         "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  //     },
+  //    {
   //       title: "Test Article 2",
   //       createdAt: new Date(),
   //       description: "Test description 2",
